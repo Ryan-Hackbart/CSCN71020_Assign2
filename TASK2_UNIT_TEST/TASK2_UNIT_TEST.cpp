2f4b6ce029db0d2e4b3a09ded19ceb3ff42ded38
Ryan Hackbart <Rhackbart6884@conestogac.on.ca>
2024-10-16 12:47:59 -0400

    added & pointers to TASK2_UNIT_TEST that I forgot to include in the original unit test

--- TASK2_UNIT_TEST/TASK2_UNIT_TEST.cpp
+++ TASK2_UNIT_TEST/TASK2_UNIT_TEST.cpp
@@ -15,21 +15,21 @@ namespace TASK2UNITTEST
 		TEST_METHOD(SetLengthTest1) {
 			// testing functionality of the setLength function 8 and length, expecting length = 8 
 			int length = 0;
-			setLength(8, length);
+			setLength(8, &length);
 			Assert::AreEqual(8, length);
 		}
 
 		TEST_METHOD(SetLengthTest2) {
 			// testing functionality of the setLength function 99 and length, expecting length = 99
 			int length = 0;
-			setLength(99, length);
+			setLength(99, &length);
 			Assert::AreEqual(99, length);
 		}
 
 		TEST_METHOD(SetLengthTest3) {
 			// testing functionality of the setLength function 1 and length, expecting length = 1 
 			int length = 0;
-			setLength(1, length);
+			setLength(1, &length);
 			Assert::AreEqual(1, length);
 		}
 
@@ -38,21 +38,21 @@ namespace TASK2UNITTEST
 		TEST_METHOD(SetWidthTest1) {
 			// testing functionality of the GetArea function 5 and width, expecting width = 5 
 			int width = 0;
-			setWidth(5, width);
+			setWidth(5, &width);
 			Assert::AreEqual(5, width);
 		}
 
 		TEST_METHOD(SetWidthTest2) {
 			// testing functionality of the GetArea function 99 and width, expecting width = 99 
 			int width = 0;
-			setWidth(99, width);
+			setWidth(99, &width);
 			Assert::AreEqual(99, width);
 		}
 
 		TEST_METHOD(SetWidthTest3) {
 			// testing functionality of the GetArea function 1 and width, expecting width = 1 
 			int width = 0;
-			setWidth(1, width);
+			setWidth(1, &width);
 			Assert::AreEqual(1, width);
 		}
 	};
